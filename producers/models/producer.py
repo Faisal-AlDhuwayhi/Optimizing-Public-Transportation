29f7a11171c1b8141ba15ff18d3adf3e6598a5c8
Faisal Aldhuwayhi <alduohy@hotmail.com>
2024-09-01 21:57:01 +0300

    Upload producers

---
+++ producers/models/producer.py
@@ -0,0 +1,95 @@
+"""Producer base-class providing common utilites and functionality"""
+import logging
+import time
+
+
+from confluent_kafka import avro
+from confluent_kafka.admin import AdminClient, NewTopic
+from confluent_kafka.avro import AvroProducer
+
+logger = logging.getLogger(__name__)
+
+
+class Producer:
+    """Defines and provides common functionality amongst Producers"""
+
+    # Tracks existing topics across all Producer instances
+    existing_topics = set([])
+
+    def __init__(
+        self,
+        topic_name,
+        key_schema,
+        value_schema=None,
+        num_partitions=1,
+        num_replicas=1,
+    ):
+        """Initializes a Producer object with basic settings"""
+        self.topic_name = topic_name
+        self.key_schema = key_schema
+        self.value_schema = value_schema
+        self.num_partitions = num_partitions
+        self.num_replicas = num_replicas
+
+
+        # Configure the broker properties
+        self.broker_properties = {
+            "kafka_broker_url": "localhost:9092",
+            "schema_registry_url": "http://localhost:8081",
+        }
+
+        # If the topic does not already exist, try to create it
+        if self.topic_name not in Producer.existing_topics:
+            self.create_topic()
+            Producer.existing_topics.add(self.topic_name)
+
+        # Configure the AvroProducer
+        self.producer = AvroProducer(
+            config={
+                "schema.registry.url": self.broker_properties['schema_registry_url'],
+                "bootstrap.servers": self.broker_properties['kafka_broker_url']
+            },
+            default_key_schema=self.key_schema,
+            default_value_schema=self.value_schema
+        )
+
+    def create_topic(self):
+        """Creates the producer topic if it does not already exist"""
+        # creates the topic for this producer if it does not already exist on the Kafka Broker.
+        admin_client = AdminClient({"bootstrap.servers": self.broker_properties['kafka_broker_url']})
+        
+        topics_metadata = admin_client.list_topics(timeout=5)
+        if self.topic_name in topics_metadata.topics:
+            logger.info(f"Topic ({self.topic_name}) is already exist")
+            return
+        
+        new_topic = NewTopic(
+            self.topic_name, 
+            num_partitions=self.num_partitions,
+            replication_factor=self.num_replicas
+        )
+        
+        futures = admin_client.create_topics([new_topic])
+        
+        for topic, future in futures.items():
+            try:
+                future.result()
+                logger.info("Topic {} created".format(topic))
+            except Exception as e:
+                logger.error("Failed to create topic {}: {}".format(topic, e))
+                
+
+    def time_millis(self):
+        return int(round(time.time() * 1000))
+
+    def close(self):
+        """Prepares the producer for exit by cleaning up the producer"""
+        # cleanup code for the Producer
+        self.producer.flush()
+        self.producer.close()
+        
+        logger.info("Producer close complete")
+
+    def time_millis(self):
+        """Use this function to get the key for Kafka Events"""
+        return int(round(time.time() * 1000))
